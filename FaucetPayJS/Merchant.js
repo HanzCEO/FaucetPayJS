e27c38ef4db1f17e3efda72a80e8b59d9bbe94cd
Hanz <40712686+HanzCEO@users.noreply.github.com>
2022-02-21 11:43:50 +0700

    Updated Merchant.js
    My past was so silly, thanks Allah i'm an adult now

--- FaucetPayJS/Merchant.js
+++ FaucetPayJS/Merchant.js
@@ -1,47 +1,8 @@
-const axios = require('axios')
-
-/*
- * This greedy function doesnt work
- * because this just simply redirects
- * costumer to faucetpay's page which
- * we didn't handle, right?
- *
- * Just forget about this function,
- * let's create middlewares instead
- */
-async function createInvoice(
-	item_description,
-	price,
-	receive_as,
-	pay_with='',
-	custom="",
-	callback_url='http://0.0.0.0',
-	success_url='http://merchant.hanzhaxors.online/thank-you',
-	cancel_url='http://merchant.hanzhaxors.online/feedback'
-) {
-	try {
-		let response = await axios.post('/webscr', qs.stringify({
-			merchant_username: this.merchant_username,
-			item_description: String(item_description),
-			amount1: +price,
-			currency1: receive_as.toUpperCase(),
-			currency2: pay_with.toUpperCase(),
-			custom: String(custom),
-			callback_url: String(callback_url),
-			success_url: String(success_url),
-			cancel_url: String(cancel_url)
-		}), this.axios_config)
-
-		return response.data
-	} catch (e) {
-		console.error(e)
-		throw e
-	}
-}
+const axios = require('axios');
 
 async function validateToken(token) {
-	let response = await axios.get(`https://faucetpay.io/merchant/get-payment/${token}`, this.axios_config)
-	return response.data
+	let response = await axios.get(`https://faucetpay.io/merchant/get-payment/${token}`, this.axios_config);
+	return response.data;
 }
 
 /*
@@ -55,19 +16,19 @@ async function validateToken(token) {
 function getMiddleware(config={}) {
 	let retfun = async (req, res, next) => {
 		if (!req.body || !req.body.token) {
-			return next()
+			return next();
 		}
 
-		req.paymentData = await validateToken(req.body.token)
-		next()
+		req.paymentData = await validateToken(req.body.token);
+		next();
 	}
 
-	return retfun
+	return retfun;
 }
 
 class Merchant {
 	constructor(merchant_username) {
-		this.merchant_username = merchant_username
+		this.merchant_username = merchant_username;
 		this.fp = axios.create({
 			baseURL: 'https://faucetpay.io/merchant/',
 			method: 'POST',
@@ -75,22 +36,22 @@ class Merchant {
 				'X-FaucetPayJS': 'v0.2.0',
 				'User-Agent': 'FaucetPayJS/0.2.0'
 			}
-		})
+		});
 		this.axios_config = {
 			timeout: 0
-		}
+		};
 	}
 }
 
-Merchant.prototype.createInvoice = createInvoice
-Merchant.prototype.makeInvoice = createInvoice
-Merchant.prototype.generateInvoice = createInvoice
-Merchant.prototype.requestInvoice = createInvoice
+Merchant.prototype.createInvoice = createInvoice;
+Merchant.prototype.makeInvoice = createInvoice;
+Merchant.prototype.generateInvoice = createInvoice;
+Merchant.prototype.requestInvoice = createInvoice;
 
-Merchant.prototype.validateToken = validateToken
-Merchant.prototype.checkToken = validateToken
-Merchant.prototype.isTokenValid = validateToken
+Merchant.prototype.validateToken = validateToken;
+Merchant.prototype.checkToken = validateToken;
+Merchant.prototype.isTokenValid = validateToken;
 
-Merchant.prototype.getMiddleware = getMiddleware
+Merchant.prototype.getMiddleware = getMiddleware;
 
-module.exports = Merchant
+module.exports = Merchant;
